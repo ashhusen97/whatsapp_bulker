6eb650ef7dbd6288b818c2f5a44769e21f917ab7
Aashir Hussain <aashirhussain@Aashirs-MacBook-Pro.local>
2025-08-27 23:43:40 +0500

    Fixed frontend changes

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -5,7 +5,7 @@ import ThemeProvider from "./utils/ThemeContext";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import { AppProvider } from "./context/AppContext";
-
+console.log("first");
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
